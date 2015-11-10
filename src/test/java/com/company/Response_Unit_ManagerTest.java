99e875373f8656435de2f71cd9e9df2cd06a9cdb
Ben Hoelzel <ben_hoelzel@cox.net>
2015-11-09 20:24:25 -0800

    Update to ResponseUnitManager Test

--- src/test/java/com/company/Response_Unit_ManagerTest.java
+++ src/test/java/com/company/Response_Unit_ManagerTest.java
@@ -12,11 +12,9 @@ import sun.org.mozilla.javascript.internal.EcmaError;
 
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-/**
- * Created by kazzdingo on 11/4/15.
- */
 public class Response_Unit_ManagerTest {
     String test_id = "1001";
     Response_Unit test_response_unit;
@@ -42,16 +40,6 @@ public class Response_Unit_ManagerTest {
         test_subject.Add_Response_Unit(test_response_unit);
     }
 
-
-//    @Test
-//    public void TestAddingGoodResponseUnit() throws Exception {
-//        //Testing of Location has been completed prior
-//        Location test_location = new Location(-80.0f, -100.0f);
-//        //Testing of Response_Unit has been completed prior
-//        Response_Unit test_response_unit = new Response_Unit(test_id, test_location);
-//        test_subject.Add_Response_Unit(test_response_unit);
-//    }
-
     //Positive path getter tests
     /*
     Test Case ID: 6.02
@@ -68,7 +56,7 @@ public class Response_Unit_ManagerTest {
     Test Case ID: 6.03
     Purpose: Test Response_Unit getter
     Preconditions: Test_Object has been initialized with Response_Unit having ID of "1001"
-    Expected result: Return true
+    Expected result: The returned Response_Unit is the same as the test_response_unit built in setUp()
     */
     @Test
     public void TestResponseUnitGetter(){
@@ -77,33 +65,57 @@ public class Response_Unit_ManagerTest {
         assertThat(retrived_unit, is(equalTo(test_response_unit)));
     }
 
-
-
-//TODO: Test bool
-}
-
-
-// Testing Add_Response_Unit
+    //Negative path getter tests
     /*
-    Test Case ID: 6.01
-    Purpose: “Testing if the New_Unit is NULL that the program will not crash”
+    Test Case ID: 6.04
+    Purpose: “Testing if the Response_Unit_Exists returns False when Response_Unit does not Exists"
+    Preconditions: Unit_ID does not match an existing/pre-initialized Response_Unit.
+    Expected Result: False Boolean is returned
+    */
+    @Test
+    public void TestResponseIdExistWithBadId(){
+        assertFalse(test_subject.Response_Unit_Exists("999"));
+    }
+
+    /*
+    Test Case ID: 6.05
+    Purpose: “Testing if the Response_Unit_Named runs when Unit_ID does not match existing Response_Unit"
+    Preconditions: Unit_ID does not matches an existing Response_Unit.
+    Expected Result: null returned
+    */
+    @Test
+    public void TestResponseUnitGetterWithBadId(){
+        Response_Unit retrived_unit;
+        retrived_unit = test_subject.Response_Unit_Named("999");
+        assertThat( retrived_unit, is( equalTo(null)));
+     }
+
+    //Negative path setter test
+    /*
+    Test Case ID: 6.06
+    Purpose: "Adding a null object to the Response_Unit_Manager”
     Preconditions: New_Unit is null
     Expected Result: Null_Object_Exception is thrown.
     */
+    @Test (expected = Null_Object_Exception.class)//Duplicate_Item_Exception not expected.
+    public void TestResponseSetWithNullResponseUnit() throws Null_Object_Exception, Duplicate_Item_Exception {
+        Response_Unit a_unit = null;
+        test_subject.Add_Response_Unit( a_unit );
+    }
 
 
-
-// Testing the smallest Unit (referencing test sequence)
     /*
-    Test Case ID: 6.02
+    Test Case ID: 6.07
     Purpose: “Testing if the New_Unit has a duplicate ID that the program will not crash”
     Preconditions: New_Unit has the same ID as an existing unit
     Expected Result: Duplicate_Item_Exception is thrown.
     */
-
-
-
-
+    @Test (expected = Duplicate_Item_Exception.class)//Duplicate_Item_Exception not expected.
+    public void TestResponseSetWithDuplicateResponseUnit() throws Null_Object_Exception, Duplicate_Item_Exception {
+        //test_response_unit already exist in the
+        Response_Unit a_new_unit = test_response_unit;
+        test_subject.Add_Response_Unit( a_new_unit );
+    }
 
     /*
     Test Case ID: 6.03
@@ -140,30 +152,14 @@ public class Response_Unit_ManagerTest {
     Expected Result: New Response_Unit with Unit_ID=3.14 is inserted into Response_Unit_DB.
     */
 
-//Testing Response_Unit_Named
-    /*
-    Test Case ID: 6.08
-    Purpose: “Testing if the Response_Unit_Named returns valid Response_Unit type"
-    Preconditions: Unit_ID matches an exisiting/preinitilized Reponse_Unit.
-    Expected Result: Valid Reponse_Unit is returned
-    */
-    /*
-    Test Case ID: 6.09
-    Purpose: “Testing if the Response_Unit_Named runs when Unit_ID does not match existing Reponse_Unit"
-    Preconditions: Unit_ID does not matches an exisiting Reponse_Unit.
-    Expected Result: No Reponse_Unit returned
-    */
+}
 
-//Testing Response_Unit_Exists
-    /*
-    Test Case ID: 6.10
-    Purpose: “Testing if the Response_Unit_Exists returns True when Reponse_Unit Exists"
-    Preconditions: Unit_ID matches an exisiting/preinitilized Reponse_Unit.
-    Expected Result: True Boolean is returned
-    */
-    /*
-    Test Case ID: 6.11
-    Purpose: “Testing if the Response_Unit_Exists returns False when Reponse_Unit does not Exists"
-    Preconditions: Unit_ID does not match an exisiting/preinitilized Reponse_Unit.
-    Expected Result: False Boolean is returned
-    */
+
+
+
+
+
+
+
+
+
